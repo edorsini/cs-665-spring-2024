b3d58f47ffdb92fd4a5e24c835878dc0a7d21727
Ed Orsini <edorsini@edsmbp1inch2021.lan>
2024-02-15 22:17:13 -0500

    add new facade, factory, iterator, singleton, and state

---
+++ factory/method/Main.java
@@ -0,0 +1,22 @@
+package patterns.factory.method;
+
+// Design principles:
+// Open Closed principle: classes, methods, or functions should be Open for extension and closed for modification.
+//   The key benefit of this design principle is that already tried and tested code is not touched which means they
+//   won’t break.
+
+// Factory method allows you to vary the products you are creating...
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        PizzaStore chicagoStore = new ChicagoStylePizzaStore();
+        PizzaStore nyStore = new NYStylePizzaStore();
+
+        chicagoStore.createPizza("cheese");
+        nyStore.createPizza("cheese");
+    }
+
+
+}
